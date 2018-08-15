0e912950001816b7bcbdc0a0053470ddb105345d
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2018-08-15 22:44:42 +0530

    second commit

--- src/integration.test.js
+++ src/integration.test.js
@@ -24,15 +24,45 @@ describe('guessWord action dispatcher', () => {
             expect(newState).toEqual(expectedState);
         });
         test('updates state correctly for successful guess', () => {
-            
+            store.dispatch(guessWord(secretWord));
+            const newState = store.getState();
+            const expectedState = {
+                ...initialState,
+                success: true,
+                guessedWords: [{
+                    guessedWord : secretWord,
+                    letterMatchCount: 5
+                }]
+            };
+            expect(newState).toEqual(expectedState);
         });
     });
     describe('some guessed words', () => {
+        const guessedWords = [ { guessedWord: 'agile', letterMatchCount: 1 }];
+        const initialsState = { guessedWords, secretWord }
+        let store;
+        beforeEach(() => {
+            store = storeFactory(initialsState);
+        });
         test('updates state correctly for unsuccessful guess', () => {
-
+            store.dispatch(guessWord(unsuccessfulGuess));
+            const newState = store.getState();
+            const expectedState = {
+                secretWord,
+                success: false,
+                guessedWords : [...guessedWords, { guessedWord: unsuccessfulGuess, letterMatchCount: 3 } ]
+            };
+            expect(newState).toEqual(expectedState);
         });
         test('updates state correctly for successful guess', () => {
-            
+            store.dispatch(guessWord(secretWord));
+            const newState = store.getState();
+            const expectedState = {
+                secretWord,
+                success: true,
+                guessedWords : [...guessedWords, { guessedWord: secretWord, letterMatchCount: 5 } ]
+            };
+            expect(newState).toEqual(expectedState);
         });
     });
 });
